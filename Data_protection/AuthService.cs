7cfcbf5de93aef54944e8241d2aa7a53b78a72d2
Dmitry Med <dmedvedev2002@gmail.com>
2023-11-17 21:47:21 +0300

    first commit

---
+++ Data_protection/AuthService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+internal class AuthService
+{
+    private readonly UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public void RegisterUser(string username, string password, string role)
+    {
+        // Хешируем пароль перед сохранением в базу данных
+        string passwordHash = HashPassword(password);
+
+        // Создаем нового пользователя
+        var user = new User { Username = username, PasswordHash = passwordHash , Role = role};
+
+        // Сохраняем пользователя в базу данных
+        userRepository.CreateUser(user);
+    }
+
+    public bool AuthenticateUser(string username, string password, string requiredRole = null)
+    {
+        // Получаем пользователя из базы данных по имени пользователя
+        var user = userRepository.GetUserByUsername(username);
+
+        if (user != null && VerifyPassword(password, user.PasswordHash))
+        {
+            // Проверяем, совпадает ли роль пользователя с требуемой ролью (если требуется)
+            if (requiredRole == null || string.Equals(user.Role, requiredRole, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private string HashPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            // Хешируем пароль с использованием SHA-256
+            byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            // Преобразуем байты в строку для хранения в базе данных
+            return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+        }
+    }
+
+    private bool VerifyPassword(string password, string storedHash)
+    {
+        // Проверяем, совпадает ли хеш введенного пароля с хешем из базы данных
+        return string.Equals(HashPassword(password), storedHash, StringComparison.OrdinalIgnoreCase);
+    }
+}
---
+++ Data_protection/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Data_protection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbPath = "D:\\My_programms\\C#\\Data_protection/database.db";
+            var userRepository = new UserRepository(dbPath);
+            var authService = new AuthService(userRepository);
+
+            // Пример регистрации пользователя
+            authService.RegisterUser("Dmitry_Medvedev", "admin_password", "admin");
+            authService.RegisterUser("Ivan", "1234" , "User");
+
+            // Пример аутентификации пользователя
+            bool isAuthenticated = authService.AuthenticateUser("Ivan", "1234", "User");
+
+            if (isAuthenticated)
+            {
+                Console.WriteLine($"Пользователь успешно аутентифицирован.");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка аутентификации. Пользователь не найден или введен неверный пароль.");
+            }
+        }
+    }
+}
